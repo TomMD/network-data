14f4e8cf2fbfd0a6b9b7f9802724403d02f2c580
thomas.dubuisson <thomas.dubuisson@gmail.com>
2009-03-01 16:53:56 -0800

    Fixed the Header class definition
    Ignore-this: 22a9ea29422de62d00a6f501a197fb2a
    
    darcs-hash:20090302005356-cef97-8870729a951845edb383e2a4e4011c49a011f545.gz

--- Data/Header.hs
+++ Data/Header.hs
@@ -1,18 +1,43 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
-module Data.Header where
+module Data.Header
+	( L3Header (..)
+	, L3Address (..)
+	) where
 
 import Data.Binary (encode, Binary)
 import qualified Data.ByteString.Lazy as B
 import Data.CSum as C
 
-class (Binary h) => Header h a | h -> a where
-	zeroChecksum :: h -> h
-	zeroChecksum = id
+-- |A class of network headers that assumes a checksum is present.
+class (Binary h, L3Address a) => L3Header h a | h -> a where
+	-- |Returns the checksum from the header
 	getChecksum :: h -> CSum
-	computeChecksum :: h -> B.ByteString -> CSum
-	computeChecksum h _ = C.csum16 (encode (zeroChecksum h))
-	valid :: h -> Bool
-	valid h = computeChecksum h B.empty == getChecksum h
+
+	-- |Sets the checksum in the header
+	setChecksum :: h -> CSum -> h
+
+	-- |Returns a 'source' for the header.
 	src :: h -> a
+
+	-- |Returns a 'destination' for the header.
 	dst :: h -> a
-	
+
+	-- |Computes the checksum
+	-- |Returns a header with all the same fields except the checksum is zeroed
+	zeroChecksum :: h -> h
+	zeroChecksum h = setChecksum h 0
+
+	computeChecksum :: h -> CSum
+	computeChecksum h = C.csum16 (encode (zeroChecksum h))
+
+	fillChecksum :: h -> h
+	fillChecksum h = setChecksum h (computeChecksum h)
+
+	-- |Returns True iff the checksum is valid
+	valid :: h -> Bool
+	valid h = computeChecksum h == getChecksum h
+
+-- |A class of network addresses that assumes there is a 'broadcast' concept.
+class (Binary a, L3Header h) => L3Address a h | a -> h where
+	localBroadcast :: a -> a
+	globalBroadcast :: a
