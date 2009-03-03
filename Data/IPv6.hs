7f357c14baa85a55670c1b5326b7b0dd3da9f203
thomas.dubuisson <thomas.dubuisson@gmail.com>
2009-03-03 01:38:15 -0800

    Pretty printing and parsing
    Ignore-this: 2f0fa70bb57a82c914eb0b8c0b4c2420
    
    darcs-hash:20090303093815-cef97-ca67385e526a101df8dd7bbe3cdf41d72faedb5d.gz

--- Data/IPv6.hs
+++ Data/IPv6.hs
@@ -4,11 +4,17 @@ module Data.IPv6
 	( IPv6 (..)
 	) where
 
-import Data.ByteString.Lazy as B
+import Control.Monad (sequence)
+import qualified Data.ByteString.Lazy as B
 import Data.Binary
 import Data.Binary.Put
 import Data.Binary.Get
 import Data.Bits
+import Numeric (showHex)
+import Text.PrettyPrint
+import Text.PrettyPrint.HughesPJClass
+import Text.ParserCombinators.Parsec as P
+import Data.Maybe (maybeToList)
 
 gW8 = getWord8 >>= return . fromIntegral
 gW16 = getWord16be >>= return . fromIntegral
@@ -17,7 +23,7 @@ pW8 = putWord8 . fromIntegral
 pW16 = putWord16be . fromIntegral
 pW32 = putWord32be . fromIntegral
 
-data IPv6 = IPv6 B.ByteString deriving (Eq, Ord, Show)
+data IPv6 = IPv6 B.ByteString deriving (Eq, Ord, Show, Read)
 
 instance Binary IPv6 where
 	put (IPv6 b) = putLazyByteString b
@@ -61,3 +67,44 @@ data IPv6Ext = E deriving (Eq, Ord, Show)
 instance Binary IPv6Ext where
 	get = return E
 	put _ = return ()
+
+-- TODO: Header and Address instanes
+
+instance Pretty IPv6 where
+	pPrint (IPv6 i) = cat . alternate colon . map (pHex 2) $ (B.unpack i)
+
+-- Until 'hex' is part of the pretty printer
+pHex nr n = text $ pad nr $ showHex n ""
+  where
+  pad n s = let l = length s in if l < n then replicate (n - l) '0' ++ s else drop (l - n) s
+
+alternate :: a -> [a] -> [a]
+alternate f xs = go xs
+  where
+  go (a:b:c:xs) = a : b : f : go (c:xs)
+  go x = x
+
+second :: GenParser Char st [String]
+second = do
+		P.char ':'
+		x <- many1 hexDigit
+		xs <- second
+		return (x : xs)
+	 <|>
+		return []
+
+first :: GenParser Char st [String]
+first = do
+	x <- many1 hexDigit
+	(do	P.char ':'
+		xs <- first
+		return (x:xs)
+	 <|> return [x])
+	<|> return []
+
+ipv6 :: GenParser Char st IPv6
+ipv6 = do error "blah"
+  where
+  replace :: (Eq a) => a -> [a] -> [a] -> [a]
+  replace _ _ [] = []
+  replace n r (x:xs) = if n == x then r ++ xs else x : replace n r xs
