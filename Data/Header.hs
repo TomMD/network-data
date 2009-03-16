52707113d470faa6f9a8d6886f1b86428e6ec41b
thomas.dubuisson <thomas.dubuisson@gmail.com>
2009-03-16 15:17:00 -0700

    Checksum type should be a typeclass parameter, not assumed.
    Ignore-this: cb8289caf5437fffd14aa41d97889d42
    
    darcs-hash:20090316221700-cef97-b526e4e700a071beecb900126b5708b244fefd3f.gz

--- Data/Header.hs
+++ Data/Header.hs
@@ -9,12 +9,12 @@ import qualified Data.ByteString.Lazy as B
 import Data.CSum as C
 
 -- |A class of network headers that assumes a checksum is present.
-class (Binary h) => L3Header h a | h -> a, a -> h where
+class (Binary h) => L3Header h a c | h -> a, a -> h, h -> c where
 	-- |Returns the checksum from the header
-	getChecksum :: h -> CSum
+	getChecksum :: h -> c
 
 	-- |Sets the checksum in the header
-	setChecksum :: h -> CSum -> h
+	setChecksum :: h -> c -> h
 
 	-- |Returns a 'source' for the header.
 	src :: h -> a
@@ -22,20 +22,21 @@ class (Binary h) => L3Header h a | h -> a, a -> h where
 	-- |Returns a 'destination' for the header.
 	dst :: h -> a
 
-	-- |Computes the checksum
 	-- |Returns a header with all the same fields except the checksum is zeroed
 	zeroChecksum :: h -> h
 	zeroChecksum h = setChecksum h 0
 
-	computeChecksum :: h -> CSum
+	-- |Computes the checksum
+	computeChecksum :: h -> c
 	computeChecksum h = C.csum16 (encode (zeroChecksum h))
 
+	-- |Computes the checksum, returns a header with the proper checksum
 	fillChecksum :: h -> h
 	fillChecksum h = setChecksum h (computeChecksum h)
 
 	-- |Returns True iff the checksum is valid
 	valid :: h -> Bool
-	valid h = computeChecksum h == getChecksum h
+	valid h = zeroCSum == getChecksum h || computeChecksum h == getChecksum h
 
 -- |A class of network addresses that assumes there is a 'broadcast' concept.
 class (Binary a) => L3Address a h | a -> h, h -> a where
